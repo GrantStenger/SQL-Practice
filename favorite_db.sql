877dda5b62d8269c51d86c992e7e0e83cdcfc518
Grant Stenger <ghstenge@usc.edu>
2018-01-18 22:46:45 -0800

    formatting favorite_db.sql

--- favorite_db.sql
+++ favorite_db.sql
@@ -10,13 +10,13 @@ CREATE TABLE favorite_foods(
 CREATE TABLE favorite_songs(
 	song VARCHAR(100) NOT NULL,
 	artist VARCHAR(50) NOT NULL,
-    score INTEGER(10)
+	score INTEGER(10)
 );
 
 CREATE TABLE favorite_movies(
 	film VARCHAR(100) NOT NULL,
-    five_times BOOLEAN NOT NULL,
-    score INTEGER(10)
+	five_times BOOLEAN NOT NULL,
+	score INTEGER(10)
 );
 
 SELECT *
