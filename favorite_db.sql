5c191cea1dae67e68e724d0f3cb720769332300c
Grant Stenger <ghstenge@usc.edu>
2018-01-18 22:45:43 -0800

    Update favorite_db.sql

--- favorite_db.sql
+++ favorite_db.sql
@@ -4,12 +4,12 @@ USE favorite_db;
 
 CREATE TABLE favorite_foods(
 	food VARCHAR(50) NOT NULL,
-    score INTEGER(10)
+	score INTEGER(10)
 );
 
 CREATE TABLE favorite_songs(
 	song VARCHAR(100) NOT NULL,
-    artist VARCHAR(50) NOT NULL,
+	artist VARCHAR(50) NOT NULL,
     score INTEGER(10)
 );
 
